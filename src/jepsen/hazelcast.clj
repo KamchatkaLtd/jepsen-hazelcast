136fce34902270027131297908cef999b84663c7
Alexey Filippov <alexey@kamchatkaltd.co.uk>
2016-06-25 00:04:51 +0100

    An empty DB

--- src/jepsen/hazelcast.clj
+++ src/jepsen/hazelcast.clj
@@ -1,8 +1,23 @@
 (ns jepsen.hazelcast
-  (:require [jepsen.tests :as tests]
-            [jepsen.control :as control]))
+  (:require [clojure.tools.logging :refer :all]
+            [jepsen [db    :as db]
+                    [control :as c]
+                    [tests :as tests]]
+            [jepsen.os.debian :as debian]))
+
+(defn db
+  "Hazelcast DB for a particular version."
+  [version]
+  (reify db/DB
+    (setup! [_ test node]
+      (info node "installing HC" version))
+
+    (teardown! [_ test node]
+      (info node "tearing down HC"))))
 
 (defn hc-test
   [version]
   (assoc tests/noop-test
+  	     :os  "debian-os"
+  	     :db  (db version)
          :ssh { :username "alexey" }))
